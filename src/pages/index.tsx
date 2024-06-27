a3d215dd29d9bd86aa29768f9ca5b3e6bc202e6c
andrewhenrique2 <andrewloly06@gmail.com>
2024-06-27 19:38:15 -0300

    ADD Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-// pages/index.tsx
+import { GetServerSideProps } from "next";
 import Image from "next/image";
+
+import { useKeenSlider } from 'keen-slider/react'
+
+import { stripe } from "../lib/stripe";
 import { HomeContainer, Product } from "../styles/pages/home";
+
 import camiseta1 from '../assets/1.png'
 import camiseta2 from '../assets/2.png'
 import camiseta3 from '../assets/3.png'
 
-import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
 
 
-export default function Home() {
+export default function Home(props) {
 
   const [sliderRef] = useKeenSlider ({
     slides: {
@@ -63,4 +67,14 @@ export default function Home() {
       </Product>
     </HomeContainer>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const response = await stripe.products.list()
+  return {
+    props: {
+      list: [1,2,3]
+    }
+  } 
 }
